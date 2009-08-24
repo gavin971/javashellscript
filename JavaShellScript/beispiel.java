7cb954f80ac8f166b88a90449ca77f67f0fffef2
rschuster <rschuster@3a1dce96-a090-11de-935f-59e0e4d10108>
2009-08-24 17:09:58 +0000

    Versuch TimePeriod auf die Reihe zu kriegen

--- JavaShellScript/beispiel.java
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env jss
-#file test.java
-import javax.swing.JOptionPane;
-
-System.out.println("Hallo Welt! " + test.getText());    
-
-JOptionPane.showMessageDialog(null, "Hallo Welt! " + test.getText());
-    
--- JavaShellScript/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    private static String text = "Toller text zum testen!";
-    
-    public static String getText() {
-        return text;
-    }
-}
