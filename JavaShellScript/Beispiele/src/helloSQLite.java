4881fa6332c4cf6c285cc3a948476dc6a5612a6f
rschuster <rschuster@3a1dce96-a090-11de-935f-59e0e4d10108>
2009-08-25 14:08:16 +0000

    SQLiteJDBC lib added

---
+++ JavaShellScript/Beispiele/src/helloSQLite.java
@@ -0,0 +1,48 @@
+//#!/usr/bin/env jss
+//#option -Dfile.encoding=UTF8
+/*
+ * This example will create a sample SQLite-Database file.
+ * It is based on a sample for SQLiteJDBC from http://www.zentus.com/sqlitejdbc/
+ */
+
+import java.sql.*;
+
+/**
+ *
+ * @author robert schuster
+ */
+public class helloSQLite {
+
+  public static void main(String[] args) throws Exception {
+      Class.forName("org.sqlite.JDBC");
+      Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+      Statement stat = conn.createStatement();
+      stat.executeUpdate("drop table if exists people;");
+      stat.executeUpdate("create table people (name, occupation);");
+      PreparedStatement prep = conn.prepareStatement(
+          "insert into people values (?, ?);");
+
+      prep.setString(1, "Gandhi");
+      prep.setString(2, "politics");
+      prep.addBatch();
+      prep.setString(1, "Turing");
+      prep.setString(2, "computers");
+      prep.addBatch();
+      prep.setString(1, "Wittgenstein");
+      prep.setString(2, "smartypants");
+      prep.addBatch();
+
+      conn.setAutoCommit(false);
+      prep.executeBatch();
+      conn.setAutoCommit(true);
+
+      ResultSet rs = stat.executeQuery("select * from people;");
+      while (rs.next()) {
+          System.out.println("name = " + rs.getString("name"));
+          System.out.println("job = " + rs.getString("occupation"));
+      }
+      rs.close();
+      conn.close();
+  }
+}
+
