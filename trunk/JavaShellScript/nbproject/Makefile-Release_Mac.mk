b873c50acbf837d9e724b32efa4ec8d60b73f4fb
RxSchuster <RxSchuster@3a1dce96-a090-11de-935f-59e0e4d10108>
2009-09-14 07:28:23 +0000

    Small changes

--- trunk/JavaShellScript/nbproject/Makefile-Release_Mac.mk
+++ trunk/JavaShellScript/nbproject/Makefile-Release_Mac.mk
@@ -50,15 +50,15 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=/Users/robert/Programm/boost_1_37_0/lib/x86/libboost_filesystem-xgcc40-mt.a /Users/robert/Programm/boost_1_37_0/lib/x86/libboost_system-xgcc40-mt.a
+LDLIBSOPTIONS=../../boost_1_37_0/lib/x64/libboost_filesystem-xgcc40-mt.a ../../boost_1_37_0/lib/x64/libboost_system-xgcc40-mt.a
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
 	${MAKE}  -f nbproject/Makefile-Release_Mac.mk dist/jss
 
-dist/jss: /Users/robert/Programm/boost_1_37_0/lib/x86/libboost_filesystem-xgcc40-mt.a
+dist/jss: ../../boost_1_37_0/lib/x64/libboost_filesystem-xgcc40-mt.a
 
-dist/jss: /Users/robert/Programm/boost_1_37_0/lib/x86/libboost_system-xgcc40-mt.a
+dist/jss: ../../boost_1_37_0/lib/x64/libboost_system-xgcc40-mt.a
 
 dist/jss: ${OBJECTFILES}
 	${MKDIR} -p dist
@@ -66,19 +66,19 @@ dist/jss: ${OBJECTFILES}
 
 ${OBJECTDIR}/src/jss.o: nbproject/Makefile-${CND_CONF}.mk src/jss.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/jss.o src/jss.cpp
+	$(COMPILE.cc) -O3 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/jss.o src/jss.cpp
 
 ${OBJECTDIR}/src/cache.o: nbproject/Makefile-${CND_CONF}.mk src/cache.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/cache.o src/cache.cpp
+	$(COMPILE.cc) -O3 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/cache.o src/cache.cpp
 
 ${OBJECTDIR}/src/config.o: nbproject/Makefile-${CND_CONF}.mk src/config.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/config.o src/config.cpp
+	$(COMPILE.cc) -O3 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/config.o src/config.cpp
 
 ${OBJECTDIR}/src/preprozessor.o: nbproject/Makefile-${CND_CONF}.mk src/preprozessor.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/preprozessor.o src/preprozessor.cpp
+	$(COMPILE.cc) -O3 -s -DMACOS -I../../boost_1_37_0/include/boost-1_37 -I/System/Library/Frameworks/CoreFoundation.framework/Headers -o ${OBJECTDIR}/src/preprozessor.o src/preprozessor.cpp
 
 # Subprojects
 .build-subprojects:
