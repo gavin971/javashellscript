65b759f5b84a3c61af5b9d8e88ef4e6faefc1788
RxSchuster <RxSchuster@3a1dce96-a090-11de-935f-59e0e4d10108>
2010-04-10 20:01:56 +0000

    added a function to parse NCL output. This function can read variables printed by the NCL print function.

--- trunk/JavaShellScript/jsslib/src/jsslib/ncl/nclLauncher.java
+++ trunk/JavaShellScript/jsslib/src/jsslib/ncl/nclLauncher.java
@@ -1,6 +1,7 @@
 package jsslib.ncl;
 
 import java.io.IOException;
+import java.util.Arrays;
 import jsslib.shell.Exec;
 
 /**
@@ -14,9 +15,10 @@ public class nclLauncher {
      * @param nclscript     the script to run
      * @param path          the path where to run the script
      * @param args          the arguments to the script, inside the script available as arg1, arg2 ....
+     * @return              the output of the ncl-script
      * @throws IOException
      */
-    public static void launch(String nclscript, String path, Object ... args) throws IOException {
+    public static String runToString(String nclscript, String path, Object ... args) throws IOException {
         String arguments = "";
         //copy the args to the arguments string
         int index = 1;
@@ -33,8 +35,135 @@ public class nclLauncher {
             }
             index++;
         }
-        System.out.println("ncl " + nclscript + " " + arguments);
         String output = Exec.runToString("ncl " + nclscript + " " + arguments, path);
-        System.out.println(output);
+        return output;
+    }
+
+    /**
+     * Parse the output of a ncl-script and extract a named variable
+     *
+     * Example output:
+     *
+     * Variable: test_variable
+     * Type: integer
+     * Total Size: 8 bytes
+     *             2 values
+     * Number of Dimensions: 1
+     * Dimensions and sizes:        [2]
+     * Coordinates:
+     * (0)        525
+     * (1)        912
+     *
+     * @param varname
+     * @param output
+     * @return
+     */
+    public static Object parseOutput(String varname, String output) {
+        // find the variable in the output
+        int index = output.indexOf("Variable: " + varname);
+
+        // find the type
+        int typeindex = output.indexOf("Type: ", index);
+        int newline = output.indexOf("\n", typeindex);
+        String typestr = output.substring(typeindex+6, newline);
+        Class type = null;
+        if (typestr.equals("integer")) {
+            type = Integer.class;
+        } else if (typestr.equals("float")) {
+            type = Float.class;
+        }
+
+        // find the dimension
+        int noDindex = output.indexOf("Number of Dimensions: ", newline);
+        newline = output.indexOf("\n", noDindex);
+        int numberOfDimensions = Integer.parseInt(output.substring(noDindex+22, newline));
+        int DimsIndex = output.indexOf("Dimensions and sizes:", newline);
+        newline = output.indexOf("\n", DimsIndex);
+        String DimStr = output.substring(DimsIndex+22, newline);
+        String[] DimsStr = DimStr.split("]");
+        int[] dims = new int[numberOfDimensions];
+        //go throw the each DimsStr from back to the front until a non numeric value is found
+        for (int i=0;i<DimsStr.length;i++) {
+            int startindex = DimsStr[i].length();
+            for (int j=DimsStr[i].length()-1;j>=0;j--) {
+                if (!DimsStr[i].substring(j, DimsStr[i].length()).matches("[0-9]*"))  {
+                    startindex = j+1;
+                    break;
+                }
+            }
+            dims[i] = Integer.parseInt(DimsStr[i].substring(startindex, DimsStr[i].length()));
+        }
+
+        //parse the values
+        int coorIndex = output.indexOf("Coordinates:", newline);
+        //create a new array for the output
+        Object result = null;
+        switch (numberOfDimensions) {
+            case 1:
+                result = new Object[dims[0]];
+                for (int i0=0;i0<dims[0];i0++) {
+                    int bindex = output.indexOf(")", newline);
+                    newline = output.indexOf("\n", bindex);
+                    String value = output.substring(bindex+1,newline).trim();
+                    ((Object[])result)[i0] = parseStringToObject(value,type);
+                }
+                break;
+            case 2:
+                result = new Object[dims[0]][dims[1]];
+                for (int i0=0;i0<dims[0];i0++) {
+                    for (int i1=0;i1<dims[1];i1++) {
+                        int bindex = output.indexOf(")", newline);
+                        newline = output.indexOf("\n", bindex);
+                        String value = output.substring(bindex+1,newline).trim();
+                        ((Object[][])result)[i0][i1] = parseStringToObject(value,type);
+                    }
+                }
+                break;
+            case 3:
+                result = new Object[dims[0]][dims[1]][dims[2]];
+                for (int i0=0;i0<dims[0];i0++) {
+                    for (int i1=0;i1<dims[1];i1++) {
+                        for (int i2=0;i2<dims[2];i2++) {
+                            int bindex = output.indexOf(")", newline);
+                            newline = output.indexOf("\n", bindex);
+                            String value = output.substring(bindex+1,newline).trim();
+                            ((Object[][][])result)[i0][i1][i2] = parseStringToObject(value,type);
+                        }
+                    }
+                }
+                break;
+            case 4:
+                result = new Object[dims[0]][dims[1]][dims[2]][dims[3]];
+                for (int i0=0;i0<dims[0];i0++) {
+                    for (int i1=0;i1<dims[1];i1++) {
+                        for (int i2=0;i2<dims[2];i2++) {
+                            for (int i3=0;i3<dims[3];i3++) {
+                                int bindex = output.indexOf(")", newline);
+                                newline = output.indexOf("\n", bindex);
+                                String value = output.substring(bindex+1,newline).trim();
+                                ((Object[][][][])result)[i0][i1][i2][i3] = parseStringToObject(value,type);
+                            }
+                        }
+                    }
+                }
+                break;
+            default:
+                System.out.println("ERROR: nclLauncher.parseOutput: maximum allowed number of dimensions is 4!");
+                return null;
+        }       
+        return result;
+    }
+
+    /**
+     * Parse the content of the String to an Object of the type ObjectType
+     * @param text
+     * @param ObjectType
+     * @return
+     */
+    private static Object parseStringToObject(String text, Class ObjectType) {
+        if (ObjectType == Integer.class) return Integer.parseInt(text);
+        else if (ObjectType == Float.class) return Float.parseFloat(text);
+        else if (ObjectType == Double.class) return Double.parseDouble(text);
+        return null;
     }
 }
