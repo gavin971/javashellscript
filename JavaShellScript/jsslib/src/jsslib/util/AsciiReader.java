0d82f902b42eac4e1cf2e97de9eeaab1cd101947
RxSchuster <RxSchuster@3a1dce96-a090-11de-935f-59e0e4d10108>
2011-06-13 17:04:49 +0000

    Helper class for XML files added.

--- JavaShellScript/jsslib/src/jsslib/util/AsciiReader.java
+++ JavaShellScript/jsslib/src/jsslib/util/AsciiReader.java
@@ -3,6 +3,8 @@ package jsslib.util;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,28 +18,34 @@ public class AsciiReader extends BufferedReader {
         super(new FileReader(Filename));
     }
 
+    public AsciiReader(InputStream stream) {
+        super(new InputStreamReader(stream));
+    } 
+
     /**
      * Read a line of Text and parse it to the DataTypes in the Format Array
-     * @param Format        an array of the types Float, Double, Intger, Long or Date
+     * @param Format        an array of the types Float, Double, Intger, Long, String or Date
      *                      the default date-format is dd.MM.yy
      * @param delimiter
      * @return              null if the end of the file is reached
      */
-    public Object[] ReadLine(Object[] Format, String delimiter) {
+    public Object[] readLine(Class[] Format, String delimiter) {
         Object[] result = new Object[Format.length];
         try {
             //read a line from the file
             String line = this.readLine();
             if (line == null) return null;
             //Split the String
-            String[] words = line.split(delimiter);
+            String[] words = line.trim().split(delimiter);
             //loop over each element in the result array
-            for (int i=0;i<Format.length;i++) {
-                if (Format[i].getClass() == Float.class) result[i] = Float.parseFloat(words[i]);
-                else if (Format[i].getClass() == Double.class) result[i] = Double.parseDouble(words[i]);
-                else if (Format[i].getClass() == Integer.class) result[i] = Integer.parseInt(words[i]);
-                else if (Format[i].getClass() == Long.class) result[i] = Long.parseLong(words[i]);
-                else if (Format[i].getClass() == Date.class) {
+            for (int i=0;i<Format.length && i<words.length;i++) {
+                if (words[i].isEmpty()) continue;
+                if (Format[i] == Float.class) result[i] = Float.parseFloat(words[i]);
+                else if (Format[i] == Double.class) result[i] = Double.parseDouble(words[i]);
+                else if (Format[i] == Integer.class) result[i] = Integer.parseInt(words[i]);
+                else if (Format[i] == Long.class) result[i] = Long.parseLong(words[i]);
+                else if (Format[i] == String.class) result[i] = words[i];
+                else if (Format[i] == Date.class) {
                     SimpleDateFormat sd = new SimpleDateFormat("dd.MM.yy");
                     result[i] = sd.parse(words[i]);
                 }
