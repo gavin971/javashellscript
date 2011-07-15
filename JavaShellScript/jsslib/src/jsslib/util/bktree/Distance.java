57c1618647ebdc537506edf7d7943d597c1ad142
RxSchuster <RxSchuster@3a1dce96-a090-11de-935f-59e0e4d10108>
2011-07-15 09:30:22 +0000

    Added bktree from http://code.google.com/p/java-bk-tree/ Added class for file utilities

---
+++ JavaShellScript/jsslib/src/jsslib/util/bktree/Distance.java
@@ -0,0 +1,11 @@
+package jsslib.util.bktree;
+
+/**
+ * @author Josh Clemm
+ * Source code from http://code.google.com/p/java-bk-tree/
+ */
+public interface Distance {
+        
+        public int getDistance(Object object1, Object object2);
+
+}
